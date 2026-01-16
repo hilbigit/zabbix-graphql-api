0c1f58590967e19b1153434e5ad072c8b2932cc8
Andreas Hilbig <andreas.hilbig@hilbigit.com>
2026-01-16 20:24:08 +0100

    chore: Update `tags` field schema to use `DeviceConfig`, enhance `isDevice` type check, and adjust IntelliJ workspace

--- src/api/resolver_helpers.ts
+++ src/api/resolver_helpers.ts
@@ -2,8 +2,7 @@ import {isObjectType} from "graphql";
 import {logger} from "../logging/logger.js";
 import {Device, Host} from "../schema/generated/graphql.js";
 
-export const isDevice = (value: Host): value is Device => !!(value as Device).deviceType;
-
+export const isDevice = (value: Host | undefined): value is Device => !!(value as Device)?.deviceType;
 /*
     As a default all . - seperators within a key shall be replaced by a Capital letter of the following word
  */
